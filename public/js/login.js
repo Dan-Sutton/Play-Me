586587733c9dc438413a5a71bd0fd066e595f1ed
Dan-Sutton <dewasutton@gmail.com>
2022-01-04 15:04:40 +0000

    login function

--- public/js/login.js
+++ public/js/login.js
@@ -8,7 +8,14 @@ const password = document.querySelector("#password")
 async function handleSubmit(){
     const response = await fetch(`${url}/users/${username.value}`)
     const { payload } = await response.json();
-    console.log(payload)
+    // console.log(payload[0].password)
+
+    if(username.value === payload[0].username &&
+        password.value === payload[0].password) {
+            window.location.href = 'artist.html'
+        } else {
+            alert("Wrong Username or Password!")
+        }
 }
 
 
